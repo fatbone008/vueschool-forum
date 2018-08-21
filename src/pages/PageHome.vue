434d1682f095b3ef91ee8af113d60edbd411c19f
陈贻辉 <yihui.chen@esmart365.com>
2018-08-21 14:25:39 +0800

    咋整

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -55,11 +55,10 @@
 
 <script>
   import sourceData from '@/data'
-  import ThreadList from './ThreadList'
+  import ThreadList from '@/components/ThreadList'
 
   console.log(sourceData)
   export default {
-    name: 'HelloWorld',
     components: {
       ThreadList
     },
