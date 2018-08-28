ff7319367bed8f47114585a739cb360cfb8b7d81
陈贻辉 <yihui.chen@esmart365.com>
2018-08-28 15:29:03 +0800

    category

--- src/pages/PageForum.vue
+++ src/pages/PageForum.vue
@@ -5,7 +5,7 @@
       <div class="forum-header">
         <div class="forum-details">
           <h1>{{forum.name}}</h1>
-          <p class="text-lead">{{forum.description；}}</p>
+          <p class="text-lead">{{forum.description}}</p>
         </div>
         <a href="new-thread.html" class="btn-green btn-small">Start a thread</a>
       </div>
